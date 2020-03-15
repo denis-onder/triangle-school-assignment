89321de3423bba4c67edf33422d822152874e37c
Denis Onder <denis_onder@protonmail.com>
2020-03-15 08:05:38 +0100

    Random inputs, scripts and docs

--- README.md
+++ README.md
@@ -6,4 +6,8 @@
 1. Install [Node.js](https://nodejs.org/). Either the LTS or Current version will work.
 2. Clone this repository.
 3. Run `npm install` from the root directory of the project.
-4. Execute `node .` from the root directory of the project to start the app.
+4. Execute `npm start` from the root directory of the project to start the app.
+
+### Scripts:
+1. `npm start` - Runs the application.
+2. `npm test` - Runs the test suite.
--- index.test.js
+++ index.test.js
@@ -1,5 +1,3 @@
-const inquirer = require("inquirer");
-const questions = require("./questions");
 const { expect } = require("chai");
 const {
   checkIfTriangle,
@@ -16,44 +14,48 @@ function generateRandomInput() {
 
 let inputs = generateRandomInput();
 
-describe("Suite", () => {
+describe("Test Suite", () => {
   // Greeter
   console.log(`Input: [${inputs}]`);
   // Commence testing
-  describe("Special Inputs Case: validateInput returns false if the provided inputs are valid, otherwise it returns a list of errors.", () =>
-    it("should continue if all inputs are a number,", () =>
-      expect(validateInput(inputs)).to.eq(false)));
-  describe("Edge Case: checkIfTriangle returns true if the provided inputs can form a triangle.", () =>
-    it("should continue with execution if the result is true.", () =>
-      expect(checkIfTriangle(inputs)).to.eq(true)));
-  // Side length classification tests
-  describe("Classify Triangle By Side Length", () => {
-    const classificationResult = classifyTriangleBySideLength(inputs);
-    describe("Case 1: It should return a message that all the sides of the triangle are equal, if a, b and c are all equal.", () =>
-      it('should return "All sides of the triangle are equal."', () =>
-        expect(classificationResult).to.eq(
-          "All sides of the triangle are equal."
-        )));
-    describe("Case 2: It should return a message that the left and right sides of the triangle are equal, if a and c are equal, but they are not equal to b.", () =>
-      it('should return "The triangle has equal sides."', () =>
-        expect(classificationResult).to.eq("The triangle has equal sides.")));
-    describe("Case 3: It should return a message that all the sides of the triangle are different.", () =>
-      it('should return "The triangle has all three different sides."', () =>
-        expect(classificationResult).to.eq(
-          "The triangle has all three different sides."
-        )));
+  describe("Special Cases:", () => {
+    describe("Input Case: validateInput returns false if the provided inputs are valid, otherwise it returns a list of errors.", () =>
+      it("should continue if all inputs are a number,", () =>
+        expect(validateInput(inputs)).to.eq(false)));
+    describe("Edge Case: checkIfTriangle returns true if the provided inputs can form a triangle.", () =>
+      it("should continue with execution if the result is true.", () =>
+        expect(checkIfTriangle(inputs)).to.eq(true)));
   });
-  // Triangle type classification tests
-  describe("Classify Triangle By Type", () => {
-    const triangleType = checkTriangleType(inputs);
-    describe("Case 1: It should return a message that the triangle is acute if a² + b² are greater than c².", () =>
-      it('should return "Acute (Less than 90 degrees)".', () =>
-        expect(triangleType).to.eq("Acute (Less than 90 degrees)")));
-    describe("Case 2: It should return a message that the triangle is obtuse if a² + b² are less than c².", () =>
-      it('should return "Obtuse (Greater than 90 degrees)".', () =>
-        expect(triangleType).to.eq("Obtuse (Greater than 90 degrees)")));
-    describe("Case 3: It should return a message that the triangle is right-angle if a² + b² are equal to c².", () =>
-      it('should return "Right-angle".', () =>
-        expect(triangleType).to.eq("Right-angle")));
+  describe("Unit Tests:", () => {
+    // Side length classification tests
+    describe("Classify Triangle By Side Length", () => {
+      const classificationResult = classifyTriangleBySideLength(inputs);
+      describe("Case 1: It should return a message that all the sides of the triangle are equal, if a, b and c are all equal.", () =>
+        it('should return "All sides of the triangle are equal."', () =>
+          expect(classificationResult).to.eq(
+            "All sides of the triangle are equal."
+          )));
+      describe("Case 2: It should return a message that the left and right sides of the triangle are equal, if a and c are equal, but they are not equal to b.", () =>
+        it('should return "The triangle has equal sides."', () =>
+          expect(classificationResult).to.eq("The triangle has equal sides.")));
+      describe("Case 3: It should return a message that all the sides of the triangle are different.", () =>
+        it('should return "The triangle has all three different sides."', () =>
+          expect(classificationResult).to.eq(
+            "The triangle has all three different sides."
+          )));
+    });
+    // Triangle type classification tests
+    describe("Classify Triangle By Type", () => {
+      const triangleType = checkTriangleType(inputs);
+      describe("Case 1: It should return a message that the triangle is acute if a² + b² are greater than c².", () =>
+        it('should return "Acute (Less than 90 degrees)".', () =>
+          expect(triangleType).to.eq("Acute (Less than 90 degrees)")));
+      describe("Case 2: It should return a message that the triangle is obtuse if a² + b² are less than c².", () =>
+        it('should return "Obtuse (Greater than 90 degrees)".', () =>
+          expect(triangleType).to.eq("Obtuse (Greater than 90 degrees)")));
+      describe("Case 3: It should return a message that the triangle is right-angle if a² + b² are equal to c².", () =>
+        it('should return "Right-angle".', () =>
+          expect(triangleType).to.eq("Right-angle")));
+    });
   });
 });
